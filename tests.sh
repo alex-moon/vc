c5daf69ddad20901f4fe0c708408c7ca81bb45e1
Alex Moon <alex@placechangers.co.uk>
2021-08-04 21:37:48 +0100

    VC-1 - OK yeah so basically this...

--- tests.sh
+++ tests.sh
@@ -2,7 +2,7 @@
 
 mode=$1
 if [[ -z "$mode" ]]; then
-    mode=images
+    mode=zoom3d
 fi
 
 IFS=$'\n'
@@ -20,22 +20,36 @@ function images() {
 }
 
 i=0
+function generate() {
+    padded_count=$(printf "%04d" "$i")
+    python3 generate.py -p="$@" \
+        -opt="$OPTIMISER" \
+        -lr=$LR \
+        -i=$MAX_ITERATIONS \
+        -se=$MAX_ITERATIONS \
+        --seed=$SEED \
+        -ii="input.png" \
+        -o="input.png"
+    cp "input.png" "input-$padded_count.png"
+    (( i ++ ))
+}
+
 function zoom() {
     for (( j=1; j<=$MAX_EPOCHS; j++ )); do
-        padded_count=$(printf "%04d" "$i")
-        python3 generate.py -p="$@" \
-            -opt="$OPTIMISER" \
-            -lr=$LR \
-            -i=$MAX_ITERATIONS \
-            -se=$MAX_ITERATIONS \
-            --seed=$SEED \
-            -ii="$FILENAME" \
-            -o="$FILENAME"
-        cp "$FILENAME" "$FILENAME_NO_EXT"-"$padded_count"."$FILE_EXTENSION"
+        generate
 
         # scale, rotate, translate: <coords from>, <scale (multiple)>, <rotate (degrees)>, <coords to>
-        convert "$FILENAME" -distort SRT "0,0 1.01 1 10,0" -gravity center "$FILENAME"
-        (( i ++ ))
+        convert "input.png" -distort SRT "0,0 1.01 1 10,0" -gravity center "input.png"
+    done
+}
+
+function zoom3d() {
+    for (( j=1; j<=$MAX_EPOCHS; j++ )); do
+        generate
+
+        # scale, rotate, translate: <coords from>, <scale (multiple)>, <rotate (degrees)>, <coords to>
+        # convert "input.png" -distort SRT "0,0 1.01 1 10,0" -gravity center "input.png"
+        python3 3d_photo_inpainting/main.py --config 3d_photo_inpainting/argument.yml
     done
 }
 
@@ -50,5 +64,5 @@ for test in $(cat tests.txt); do
 done
 
 if [[ "$mode" -eq "zoom" ]]; then
-    ffmpeg -y -i "$FILENAME_NO_EXT"-%04d."$FILE_EXTENSION" -b:v 8M -c:v h264_nvenc -pix_fmt yuv420p -strict -2 -filter:v "minterpolate='mi_mode=mci:mc_mode=aobmc:vsbmc=1:fps=60'" video-$(date -Iseconds | sed 's/[^0-9]/-/g').mp4
+    ffmpeg -y -i "input-%04d.png" -b:v 8M -c:v h264_nvenc -pix_fmt yuv420p -strict -2 -filter:v "minterpolate='mi_mode=mci:mc_mode=aobmc:vsbmc=1:fps=60'" video-$(date -Iseconds | sed 's/[^0-9]/-/g').mp4
 fi
