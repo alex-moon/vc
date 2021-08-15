0294e95b3a83d748f9b0bc81fd24fe2e7196d65b
Alex Moon <alex@placechangers.co.uk>
2021-08-15 17:36:21 +0100

    VC-2 - working! :)

--- vc/service/generation.py
+++ vc/service/generation.py
@@ -3,6 +3,5 @@ from vc.value_object import GenerationSpec
 
 class GenerationService:
     def handle(self, spec: GenerationSpec):
-        import ipdb;ipdb.set_trace()
         # here you would do stuff with the specification
         pass
