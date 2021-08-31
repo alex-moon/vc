aaa8ec3861018f61a765faf51b6bdcfa049e63cf
Alex Moon <alex@placechangers.co.uk>
2021-08-31 13:57:13 +0100

    VC-7 - even more info! >:)

--- vc/service/helper/diagnosis.py
+++ vc/service/helper/diagnosis.py
@@ -1,22 +1,74 @@
+import os
+import logging
 from datetime import datetime
 
 from guppy import hpy
 
 
 class DiagnosisHelper:
+    LOG_FILE_PATH = '/var/log/vc'
     hpy = None
+    logging = False
+    is_debug = False
 
     @classmethod
     def diagnose(cls, description: str):
+        if not os.getenv('DEBUG', False):
+            return
+
+        bar = 80 * '='
+
         if cls.hpy is None:
             cls.hpy = hpy()
+            cls.debug(bar)
+            cls.debug('PRE SETREF DIAGNOSIS')
+            cls.debug(bar)
+            cls.debug(cls.hpy.heap())
+            cls.debug(bar)
             cls.hpy.setref()
-        # rows, columns = os.popen('stty size', 'r').read().split()
-        columns = 80
-        bar = columns * '='
-        now = '%s' % datetime.now()
-        print(bar)
-        print(now, 'DIAGNOSIS:', description.upper())
-        print(bar)
-        print(cls.hpy.heap())
-        print(bar)
+
+        cls.debug(bar)
+        cls.debug('DIAGNOSIS:', description.upper())
+        cls.debug(bar)
+        cls.debug(cls.hpy.heap())
+        cls.debug(bar)
+
+    @classmethod
+    def debug(cls, *args):
+        msg = cls.get_message('DEBUG', *args)
+        logging.debug(msg)
+        if cls.is_debug:
+            print(msg)
+
+    @classmethod
+    def log(cls, *args):
+        msg = cls.get_message('LOG', *args)
+        logging.info(msg)
+        print(msg)
+
+    @classmethod
+    def get_message(cls, tag, *args):
+        cls.init()
+        return '%s: %s: %s' % (
+            datetime.now().isoformat(timespec='seconds'),
+            tag,
+            ' '.join(map(lambda a: '%s' % a, args))
+        )
+
+    @classmethod
+    def init(cls):
+        cls.is_debug = os.getenv('DEBUG', False)
+
+        if cls.logging is False:
+            logging.basicConfig(
+                filename=os.path.join(
+                    cls.LOG_FILE_PATH,
+                    'vc.%s.log' % os.getenv('SERVICE', 'service')
+                ),
+                level=(
+                    logging.DEBUG
+                    if cls.is_debug
+                    else logging.INFO
+                )
+            )
+            cls.logging = True
