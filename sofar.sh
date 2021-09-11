fbf880dd80b55c6d886ac214e0d3a2dcc9f7efd7
Ubuntu <ubuntu@ip-172-31-22-88.eu-west-1.compute.internal>
2021-09-11 23:54:44 +0000

    VC-3 - couple of things for current run

--- sofar.sh
+++ sofar.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-ffmpeg -y -i "steps/%04d.png" -b:v 8M -c:v h264_nvenc -pix_fmt yuv420p -strict -2 -filter:v "minterpolate='mi_mode=mci:mc_mode=aobmc:vsbmc=1:fps=60'" output-$(date -Iseconds | sed 's/[^0-9]/-/g').mp4
+ffmpeg -y -i "steps/%04d.png" -b:v 8M -c:v h264_nvenc -pix_fmt yuv420p -strict -2 -filter:v "minterpolate='mi_mode=mci:mc_mode=aobmc:vsbmc=1:fps=60'" sofar.mp4
 
