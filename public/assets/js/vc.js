5a0a6e16e21077c9f64477bbc50dfd07712cc3f3
Alex Moon <alex@placechangers.co.uk>
2021-09-12 21:27:54 +0100

    VC-11 - autorefresh >:)

--- public/assets/js/vc.js
+++ public/assets/js/vc.js
@@ -1,13 +1,36 @@
 function Vc() {
     this.service = new Service(this);
-    this.refresh();
-
-    // poll every ten seconds
-    window.setTimeout(this.refresh.bind(this), 10000);
+    this.refreshAndSetTimeout();
 }
 Object.assign(Vc.prototype, {
+    refreshInterval: 10000,
+    autoRefresh: true,
+    timeout = null,
     create(raw) {
         this.service.create(raw, this.draw.bind(this));
+    },
+    clearTimeout() {
+        window.clearTimeout(this.timeout);
+    },
+    setTimeout() {
+        this.timeout = window.setTimeout(
+            this.refreshAndSetTimeout.bind(this),
+            this.refreshInterval
+        );
+    },
+    refreshAndSetTimeout() {
+        this.refresh();
+        if (this.autoRefresh) {
+            this.setTimeout();
+        }
+    },
+    setAutoRefresh(value) {
+        this.autoRefresh = value;
+        if (this.autoRefresh) {
+            this.setTimeout();
+        } else {
+            this.clearTimeout();
+        }
     },
     refresh() {
         this.service.refresh(this.draw.bind(this));
