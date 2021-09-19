14ef35cfd3cbc87cfe368eb270bdd00cb09acd5e
Alex Moon <alex@placechangers.co.uk>
2021-09-19 17:18:37 +0100

    fix deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,7 +7,7 @@ source venv/bin/activate
 git pull origin \$(git rev-parse --abbrev-ref HEAD)
 
 sudo service supervisor stop
-FLASK_APP=vc.app:app
+export FLASK_APP=vc.app:app
 flask db migrate
 flask db upgrade
 sudo service supervisor start
