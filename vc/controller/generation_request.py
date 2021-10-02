e9fd59a700dbee519ed11f1a34d88b5ec83a6461
Alex Moon <alex@placechangers.co.uk>
2021-10-02 23:22:00 +0100

    VC-10 - support cancel and delete

--- vc/controller/generation_request.py
+++ vc/controller/generation_request.py
@@ -1,7 +1,7 @@
 from flask import request
 from flask_restplus import fields
 from injector import inject
-from werkzeug.exceptions import NotFound, InternalServerError
+from werkzeug.exceptions import NotFound, InternalServerError, BadRequest
 
 from vc.api import api
 from vc.auth import auth
@@ -93,3 +93,34 @@ class GenerationRequestController(BaseController):
     @ns.marshal_with(private_model)
     def put(self, id_):
         return self.manager.update(id_, request.json)
+
+
+@ns.route('/<int:id_>/<string:action>')
+class GenerationRequestActionController(BaseController):
+    manager: GenerationRequestManager
+
+    @inject
+    def __init__(
+        self,
+        user_manager: UserManager,
+        manager: GenerationRequestManager,
+        *args,
+        **kwargs
+    ):
+        super().__init__(user_manager, *args, **kwargs)
+        self.manager = manager
+
+    @auth.login_required()
+    @ns.marshal_with(private_model)
+    def put(self, id_, action: str):
+        if action == 'cancel':
+            return self.cancel(id_)
+        if action == 'delete':
+            return self.soft_delete(id_)
+        raise BadRequest('Unrecognised action: %s' % action)
+
+    def cancel(self, id_):
+        return self.manager.cancel(id_)
+
+    def soft_delete(self, id_):
+        return self.manager.soft_delete(id_)
--- vc/manager/generation_request.py
+++ vc/manager/generation_request.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from vc.model.generation_request import GenerationRequest
 from vc.event import GenerationRequestCreatedEvent
 from vc.manager.base import Manager
@@ -15,3 +17,13 @@ class GenerationRequestManager(Manager):
         )
 
         return model
+
+    def cancel(self, id_):
+        model = super().find_or_throw(id_)
+        model.cancelled = datetime.now()
+        super().save(model)
+
+    def soft_delete(self, id_):
+        model = super().find_or_throw(id_)
+        model.deleted = datetime.now()
+        super().save(model)
