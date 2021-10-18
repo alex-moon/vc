7328793b6dc4bb41f949ddedcb8769a5509a2e41
Alex Moon <alex@placechangers.co.uk>
2021-10-18 21:25:02 +0100

    latest planning

--- planning.py
+++ planning.py
@@ -10,7 +10,23 @@ steps = []
 with open('planning/october.csv') as csv_file:
     reader = csv.DictReader(csv_file)
     fieldnames = reader.fieldnames
+    season = None
     for row in reader:
+        if season is not None and row['season'] != season:
+            season = row['season']
+            steps.append({
+                'texts': ['a burst of flames, a flash of burning fire'],
+                'styles': ['%s | %s' % (row['season'], row['style'])],
+                'x_velocity': to_float(row['x']),
+                'y_velocity': to_float(row['y']),
+                'z_velocity': to_float(row['z']),
+                'pan_velocity': to_float(row['pan']),
+                'tilt_velocity': to_float(row['tilt']),
+                'roll_velocity': to_float(row['roll']),
+                'upscale': True,
+                'interpolate': True,
+                'epochs': 1,
+            })
         steps.append({
             'texts': [row['text']],
             'styles': ['%s | %s' % (row['season'], row['style'])],
@@ -21,6 +37,8 @@ with open('planning/october.csv') as csv_file:
             'tilt_velocity': to_float(row['tilt']),
             'roll_velocity': to_float(row['roll']),
             'upscale': True,
+            'interpolate': True,
+            'epochs': 20,
         })
 
 print(json.dumps(steps))
