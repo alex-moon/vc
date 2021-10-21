c4b681dd7fa6564b4b599784638592d0b578e454
Alex Moon <alex@placechangers.co.uk>
2021-10-21 19:44:16 +0100

    VC-32 - add to post model

--- vc/value_object/generation_spec.py
+++ vc/value_object/generation_spec.py
@@ -24,16 +24,16 @@ class ImageSpec:
 @dataclass
 class VideoStepSpec(ImageSpec):
     iterations: int = 75
+    interpolate: bool = False
     init_iterations: int = 200
     epochs: int = 42
+    transition: int = 20
     x_velocity: float = 0.
     y_velocity: float = 0.
     z_velocity: float = 0.
     pan_velocity: float = 0.
     tilt_velocity: float = 0.
     roll_velocity: float = 0.
-    interpolate: bool = False
-    transition: int = 20
 
     schema = api.model('Video Step Spec', {
         'texts': fields.List(fields.String, default_factory=list),
@@ -43,6 +43,7 @@ class VideoStepSpec(ImageSpec):
         'interpolate': fields.Boolean(default=False),
         'init_iterations': fields.Integer(default=200),
         'epochs': fields.Integer(default=42),
+        'transition': fields.Integer(default=20),
         'x_velocity': fields.Float(default=0.),
         'y_velocity': fields.Float(default=0.),
         'z_velocity': fields.Float(default=0.),
