cb1e483b085bc91e6be8c1040acbd857b47c53fb
Alex Moon <alex@placechangers.co.uk>
2021-11-28 18:43:22 +0000

    VC-42 - don't need docker machine on linux

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-# helper functions
-function confirm() {
-    while true; do
-        read -p "[Y/n] " YESNO
-        case "$YESNO" in
-            [Yy]*|"" ) return 0;;
-            [Nn]* ) return 1;;
-            * ) printf "Try again hotshot. " ;;
-        esac
-    done
-}
-
 red="$(tput setaf 1)"
 green="$(tput setaf 2)"
 yellow="$(tput setaf 3)"
@@ -20,47 +8,7 @@ function red() { echo "${red}${@}${reset}"; }
 function green() { echo "${green}${@}${reset}"; }
 function yellow() { echo "${yellow}${@}${reset}"; }
 
-# vars
-machine=vc
-
-echo "First let's get your root login cached..."
-sudo echo "Done"
-
-# let's do it!
-if [[ -z "$(docker-machine ls -q | grep $machine)" ]]; then
-    green "Creating docker machine"
-    docker-machine create --driver virtualbox \
-        --virtualbox-memory 8192 $machine
-fi
-
-if [[ "$(docker-machine status $machine)" = "Error" ]]; then
-    green "Rebuilding docker machine"
-    docker-machine rm -f $machine
-    docker-machine start $machine
-fi
-
-if [[ "$(docker-machine status $machine)" != "Running" ]]; then
-    green "Starting docker machine"
-    docker-machine start $machine
-fi
-
-# green "Regenerating certs"
-yes | docker-machine regenerate-certs $machine
-
-green "Mounting NFS"
-docker-machine-nfs $machine -s=`pwd`
-
-green "Loading env"
-eval $(docker-machine env $machine)
-
-green "Removing docker containers"
-docker rm -f $(docker ps -a -q)
-
-ip=$(docker-machine ip $machine)
-green "Done - your IP is $ip - auto-adding to /etc/hosts..."
-sudo sed -i '' '/vc.local/d' /etc/hosts
-echo "$ip vc.local" | sudo tee -a /etc/hosts
-
+green "Building images"
 docker-compose build
 
 green Done
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-# helper functions
-function confirm() {
-    while true; do
-        read -p "[Y/n] " YESNO
-        case "$YESNO" in
-            [Yy]*|"" ) return 0;;
-            [Nn]* ) return 1;;
-            * ) printf "Try again hotshot. " ;;
-        esac
-    done
-}
-
 red="$(tput setaf 1)"
 green="$(tput setaf 2)"
 yellow="$(tput setaf 3)"
@@ -19,21 +7,6 @@ reset="$(tput sgr0)"
 function red() { echo "${red}${@}${reset}"; }
 function green() { echo "${green}${@}${reset}"; }
 function yellow() { echo "${yellow}${@}${reset}"; }
-
-# vars
-machine=vc
-
-if [[ "$(docker-machine status $machine)" != "Running" ]]; then
-    green "Starting docker machine"
-    docker-machine start $machine
-fi
-
-eval $(docker-machine env $machine)
-
-laravel_ip=$(cat /etc/hosts | grep laravel.local | grep -o "[0-9.]*")
-if [[ ! -z "$laravel_ip" ]]; then
-    sed -i '' "s/laravel.local:[0-9.]*/laravel.local:$laravel_ip/g" docker-compose.yml
-fi
 
 service=$1
 if [[ -z "$service" ]]; then
