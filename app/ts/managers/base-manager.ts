cb28884bf5d6832de01d4b0eb7bbd374e9b576e1
Alex Moon <alex@placechangers.co.uk>
2023-05-08 15:23:51 +0100

    AJ-6 - fix urls

--- app/ts/managers/base-manager.ts
+++ app/ts/managers/base-manager.ts
@@ -7,8 +7,15 @@ export abstract class BaseManager<M extends BaseModel> {
     protected host = EnvHelper.host;
     protected base_url: string;
 
+    protected full_url(url = '') {
+        if (url[0] !== '/') {
+            url = this.host + this.base_url + url;
+        }
+        return url.replace(/\/$/g, '');
+    }
+
     protected async fetch(url = ''): Promise<M | M[]> {
-        url = url[0] === '/' ? url : this.host + this.base_url + url;
+        url = this.full_url(url)
         return new Promise((resolve, reject) => {
             fetch(url, {
                 headers: {
@@ -22,7 +29,7 @@ export abstract class BaseManager<M extends BaseModel> {
     }
 
     protected async post(data: M, url = ''): Promise<M> {
-        url = this.host + this.base_url + url;
+        url = this.full_url(url)
         let status: number;
         return fetch(url, {
             method: 'POST',
@@ -43,7 +50,7 @@ export abstract class BaseManager<M extends BaseModel> {
     }
 
     protected async put(url: string, data ?: M): Promise<M> {
-        url =  this.host + this.base_url + url;
+        url = this.full_url(url);
         let status: number;
         return fetch(url, {
             method: 'PUT',
